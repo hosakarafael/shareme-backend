bc6a0833df8fb6fd849092b15b099ff686f7f1c6
rafaelhosaka <hosakarafael@gmail.com>
2024-10-23 15:01:39 +0900

    Changed application.properties to use .env

--- src/main/java/com/rafaelhosaka/shareme/SharemeApplication.java
+++ src/main/java/com/rafaelhosaka/shareme/SharemeApplication.java
@@ -1,35 +1,23 @@
 package com.rafaelhosaka.shareme;
 
-import com.rafaelhosaka.shareme.applicationuser.ApplicationUser;
 import com.rafaelhosaka.shareme.applicationuser.ApplicationUserService;
-import com.rafaelhosaka.shareme.applicationuser.Role;
-import com.rafaelhosaka.shareme.user.UserProfile;
-import com.rafaelhosaka.shareme.user.UserProfileService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import io.github.cdimascio.dotenv.Dotenv;
-import java.time.LocalDate;
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class SharemeApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("PROJECT_CLIENT_URL", dotenv.get("PROJECT_CLIENT_URL"));
-		System.setProperty("SPRING_DATA_MONGODB_URI", dotenv.get("SPRING_DATA_MONGODB_URI"));
-		System.setProperty("SPRING_DATA_MONGODB_DATABASE", dotenv.get("SPRING_DATA_MONGODB_DATABASE"));
-		System.setProperty("SPRING_MAIL_PASSWORD", dotenv.get("SPRING_MAIL_PASSWORD"));
 		SpringApplication.run(SharemeApplication.class, args);
 	}
 
 	@Bean
 	CommandLineRunner run(ApplicationUserService userService){
 		return  args -> {
-			userService.saveRole(new Role(null, "ROLE_USER"));
-			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+//			userService.saveRole(new Role(null, "ROLE_USER"));
+//			userService.saveRole(new Role(null, "ROLE_ADMIN"));
 //
 //			userService.saveUser(new ApplicationUser(null, "rafaelhosaka@shareme.com", "1234", new ArrayList<>()));
 //			userService.saveUser(new ApplicationUser(null, "admin@shareme.com", "admin", new ArrayList<>()));
